423570cf00789476290577030dbcba6b52c45255
Lakshmanan P <122290398+Luxxgit2k4@users.noreply.github.com>
2024-07-03 22:52:43 +0530

    Update Swap_file.sh

--- Swap_file.sh
+++ Swap_file.sh
@@ -1,14 +1,18 @@
-
+#To turn off existing swap file 
 sudo swapoff /swapfile
-
+#Allocating the swap file memory to 4gb 
 sudo fallocate -l 4G /swapfile
-
+#If fallocate doesn't work use dd to extend the space
 sudo dd if=/dev/zero bs=1M count=2048 >> /swapfile
-
+#Recreate the swap space
 sudo mkswap /swapfile
-
+#Turn on the swap file
+sudo swapon /swapfile
+#To check the swap file has 4gb memory
 sudo swapon --show
-
+#you can also use this command to check
 free -h
-
+#To Make Sure the Swap File Entry is in /etc/fstab
+#Verify or add the following line to /etc/fstab. 
 /swapfile none swap sw 0 0
+#If it’s already there, you don’t need to add it again.
